c2651adbc476c1e40d0437ba497c7bf62d9b7859
Zachary Giles <zachgiles3@gmail.com>
2025-03-02 14:33:19 -0500

    Tab navigation keymaps.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -34,6 +34,12 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- Change unicode input since <C-v> pastes from clipboard
 vim.keymap.set('i', '<C-e>', '<C-v>', { desc = 'Insert unicode character' })
 
+vim.keymap.set('n', '<leader>to', '<cmd>tabnew<CR>', { desc = 'Open new tab' }) -- open new tab
+vim.keymap.set('n', '<leader>tx', '<cmd>tabclose<CR>', { desc = 'Close current tab' }) -- close current tab
+vim.keymap.set('n', '<leader>tn', '<cmd>tabn<CR>', { desc = 'Go to next tab' }) --  go to next tab
+vim.keymap.set('n', '<leader>tp', '<cmd>tabp<CR>', { desc = 'Go to previous tab' }) --  go to previous tab
+vim.keymap.set('n', '<leader>tf', '<cmd>tabnew %<CR>', { desc = 'Open current buffer in new tab' }) --  move current buffer to new tab
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
