8ea81c7901b973dc95017b0c2d2a7d25ffb742b4
Zachary Giles <zachgiles3@gmail.com>
2025-03-02 14:48:15 -0500

    Dressing plugin.

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -46,6 +46,7 @@ require('lazy').setup({
 
   require 'kickstart/plugins/bufferline',
 
+  require 'kickstart/plugins/dressing',
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
