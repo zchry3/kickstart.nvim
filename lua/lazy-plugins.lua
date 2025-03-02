0bd9c9701ad544b8a5fe388c2b7c16d3a83b35a2
Zachary Giles <zachgiles3@gmail.com>
2025-03-02 14:42:42 -0500

    Bufferline.

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -44,6 +44,8 @@ require('lazy').setup({
 
   require 'kickstart/plugins/lualine',
 
+  require 'kickstart/plugins/bufferline',
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
